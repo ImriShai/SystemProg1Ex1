ae48dfe04223d9296994dfa191f94585ebe21b99
ImriShai <imrish149@gmail.com>
2024-01-23 10:05:41 +0200

    test

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 
 int main() {
     int start,end;
-    scanf("%d",&start);
-    scanf("%d",&end);
+    scanf(" %d",&start);
+    scanf(" %d",&end);
 
     if (end < start) {
         int temp = end;
@@ -14,22 +14,22 @@ int main() {
     }
     printf("The Armstrong numbers are:");
 
-    for (int i = start; i<=end; i++) {
+    for (int i = start; i<end; i++) {
         if (isArmstrong(i))
             printf(" %d",i);
     }
     printf("\nThe Palindromes are:");
-    for (int i = start; i<=end; i++){
+    for (int i = start; i<end; i++){
         if (isPalindrome(i))
             printf(" %d",i);
     }
     printf("\nThe Prime numbers are:");
-    for (int i = start; i<=end; i++){
+    for (int i = start; i<end; i++){
         if (isPrime(i))
             printf(" %d",i);
     }
     printf("\nThe Strong numbers are:");
-    for (int i = start; i<=end; i++){
+    for (int i = start; i<end; i++){
         if (isStrong(i))
             printf(" %d",i);
     }
